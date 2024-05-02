8364c1fed2f86e886ed252d9c385a08ed23f69e7
Aliza <alizashahsavari@gmail.com>
2024-05-03 02:46:32 +0330

    Familiarity with front-end and back-end processes

---
+++ src/main/kotlin/com/aliza/sample_spring_boot/controller/UserController.kt
@@ -0,0 +1,16 @@
+package com.aliza.sample_spring_boot.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+@Controller
+class UserController {
+
+    @RequestMapping(value = ["/login"] , method = [RequestMethod.GET])
+    fun openLoginPage() :String {
+
+        return "login"
+    }
+
+}
